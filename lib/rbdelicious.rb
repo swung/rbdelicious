b3ae367f3621742502f6ccb5bb4ddef3df5585b1
jbi <jbi@jbi.localdomain>
2009-05-12 00:22:23 +0800

    Still have error after elsif

--- lib/rbdelicious.rb
+++ lib/rbdelicious.rb
@@ -59,19 +59,51 @@ module RBDelicious
 
       if url != ""
         url = "/url/#{Digest::MD5.hexdigest(url)}"
-      else if user != "" && tag != ""
+      elsif user != "" && tag != ""
         url = "/#{user}/#{tag}"
-      else if user != "" && tag == ""
+      elsif user != "" && tag == ""
         url = "/#{user}"
-      else if popular == 0 && tag == ""
+      elsif popular == 0 && tag == ""
         url = "/" 
-      else if popular == 0 && tag != ""
+      elsif popular == 0 && tag != ""
         url = "/tag/#{tag}"
-      else if popular == 1 && tag == ""
+      elsif popular == 1 && tag == ""
         url = "/popular"
-      else if popular == 1 && tag != ""
+      elsif popular == 1 && tag != ""
         url = "/popular/#{tag}"
+      else
+        url = "/"
       end
+
+      rss = hc.class.get(url)
+      posts = Posts.new(rss)
+      posts.all
     end
-  end
+
+    class Posts
+      
+      def initialize(rss)
+        @rss = rss
+        @posts = []
+      end
+
+      class Post
+        def initialize(rss_item)
+          @href = rss_item['href']
+          @time = rss_item['time']
+          @hash = rss_item['hash']
+          @tag = rss_item['tag']
+          @description = rss_item['description']
+          @extended = rss_item['extended']
+        end
+
+        attr_reader :href, :time, :hash, :tag, :desciption, :extended
+      end # class Post
+
+      def all
+        @rss[:posts][:post].each {|rss_item| @posts << Post.new(rss_itme)}
+        @posts
+      end
+    end # class Posts
+  end # class Delicious
 end
